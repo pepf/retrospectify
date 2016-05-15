a3db2552701da1837e2b9415636ab84aed2a4126
pepf <pepijnfens@gmail.com>
2016-05-15 21:27:32 +0200

    Split up components to separate files

---
+++ js/board.js
@@ -0,0 +1,30 @@
+/**
+ * Component for the sidebar with the saved boards
+ */
+SavedBoards = Vue.extend({
+
+  props: ['boards','activeBoardIndex'],
+
+  data: function() { return {
+      expanded: false,
+  } },
+
+  template: '#saved-boards',
+  
+  events: {
+    'toggle-sidebar': function(e) {
+      this.toggle();
+    }
+  },
+  methods: {
+    loadBoard: function (id ) {
+      this.$dispatch('load-board', id);
+    },
+    createBoard: function () {
+      this.$dispatch('create-board');
+    },
+    toggle: function() {
+      this.expanded = !this.expanded;
+    }
+  }
+});
---
+++ js/note.js
@@ -0,0 +1,107 @@
+/**
+ * Note component, for each individual note in a board
+ */
+Note = Vue.extend({
+
+  props: ['id','content', 'type', 'position'], //text
+
+  data: function() {
+    return {
+      nClass: {
+        active: false,
+        dragging: false,
+        neutral: false,
+        positive: false,
+        improvement: false
+      },
+      nStyle: {
+        left: ""
+      },
+
+      // Meta data
+      fontSize: 1, //em
+
+      // Dragging data
+      oldPosition: {},
+      tempPosition: {},
+      start: { x: 0, y: 0 }
+    };
+  },
+
+  template: '#custom-note-template',
+
+  events: {
+    'reset-active' : function() {
+      this.nClass.active=false;
+    }
+  },
+
+  methods: {
+    removeNote: function () {
+      this.$dispatch('remove', this["id"] );
+    },
+    incrFontSize: function() {
+      var step = 0.5, max = 2.5;
+      this.fontSize = (this.fontSize + step <= max) ? this.fontSize + step : max;
+
+    },
+    decFontSize: function() {
+      var step = 0.5, min = 0.5;
+      this.fontSize = (this.fontSize - step >= min) ? this.fontSize - step : min;
+    },
+
+    setActive: function( e ) {
+      this.nClass.active = true;
+    },
+    startDrag: function( e ) {
+      this.$dispatch("start_drag", this);
+      this.nClass.active=true;
+      this.start.x = e.pageX;
+      this.start.y = e.pageY;
+      this.oldPosition = this.position;
+      this.$on("global_mousemove", this.onMouseMove);
+    },
+    onMouseMove: function( e ) {
+      var dx = e.pageX - this.start.x,
+          dy = e.pageY - this.start.y;
+
+      if (e.buttons != 0 &&
+         document.activeElement !== this.$el.querySelector("textarea")) {
+        e.preventDefault();
+
+        if (Math.abs(dx) > 0 || Math.abs(dy) > 0) {
+          this.nClass.dragging = true;
+        } else {
+          this.nClass.dragging = false;
+        }
+
+        var newX = this.oldPosition.x + dx,
+            newY = this.oldPosition.y + dy;
+        this.nStyle.left = newX + "px";
+        this.nStyle.top = newY + "px";
+        this.position = {x: newX, y: newY };
+
+        e.stopPropagation();
+      }
+
+    },
+    stopDrag: function( e ) {
+      if( e.pageX - this.start.x === 0 &&
+          e.pageY - this.start.y === 0 ) {
+          this.$el.querySelector("textarea").focus();
+          this.nClass.active = true;
+      }
+      this.nClass.dragging = false;
+      this.$dispatch("stop_drag", this);
+      this.$off("global_mousemove");
+    }
+  },
+
+  ready: function() {
+    this.$nextTick(function () {
+      this.nStyle.left = this.position.x + "px";
+      this.nStyle.top = this.position.y + "px";
+      this.nClass[this.type] = true;
+    });
+  }
+})
