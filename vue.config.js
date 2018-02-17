e0c3a5008987e42718f9bb9ec9bb021a638b54da
Pepijn Fens <pepijn.fens@coosto.com>
2018-02-17 14:49:56 +0100

    Allow appends of scripts depending on environment

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 const { version } = require('./package.json')
 
 module.exports = {
+  baseUrl: '/retrospective/',
+
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
